b98217bbc72c9f49e276ee01d036e5f1e3c4599f
tizita1224 <133226947+tizita1224@users.noreply.github.com>
2024-10-05 00:49:45 +0300

    Add files via upload

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: 'fulfilled', value: result.value };
+      }
+      return { status: 'rejected', value: result.reason };
+    }));
+}
