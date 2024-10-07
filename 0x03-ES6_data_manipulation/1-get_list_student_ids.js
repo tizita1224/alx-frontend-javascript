9f0205b9cbdedac69fe50e88f77d16568c69f924
kase2228 <etelethiopia@gmail.com>
2024-10-08 02:19:56 +0300

    using map()

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,10 @@
 import getListStudents from "./0-get_list_students";
 
-export default function getListStudentIds() {
-    const Ids = getListStudents.map(function(studList) {
+export default function getListStudentIds(arr) {
+    if(Array.isArray(arr) !== true) {
+        return [];
+    }
+    const Ids = arr.map(function(studList) {
         return studList.id;
     });
 
