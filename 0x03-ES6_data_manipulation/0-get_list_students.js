eadd7c6a0536bbb12c394bd1d58397e861a97343
kase2228 <etelethiopia@gmail.com>
2024-10-08 01:05:32 +0300

    simple array

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+    const arr = [
+        {
+            id: 1, firstName: 'Guillaume', location: 'San Francisco'
+        }
+    ];
+    return arr;
+}
