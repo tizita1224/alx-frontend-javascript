c4a1cd859d184f69e029a0f78704a2a37a9cc7a5
tizita1224 <tizitabelachew80@gmail.com>
2024-10-08 21:56:29 +0000

    update map struct

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,13 @@
-import groceriesList from "./9-groceries_list";
+import groceriesList from './9-groceries_list';
 
 export default function updateUniqueItems(map) {
-    if(!(map instanceof Map)) {
-        throw new Error("Cannot process");
-    }
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
 
-    for(const [key, value] of map) {
-        if(value === 1) {
-            map.set(key, 100);
-        }
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
     }
+  }
 }
