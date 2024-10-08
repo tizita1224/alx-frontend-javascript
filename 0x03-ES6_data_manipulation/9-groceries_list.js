5027d2ad7468b1f68682c132428662dff9feb702
tizita1224 <tizitabelachew80@gmail.com>
2024-10-08 21:13:42 +0000

    simple map structure

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
 export default function groceriesList() {
-    const map = new Map();
-    
-    map.set("Apples", 10);
-    map.set("Tomatoes", 10);
-    map.set("Pasts", 1);
-    map.set("Rice", 1);
-    map.set("Banana", 5);
+  const map = new Map();
 
-    return map;
+  map.set('Apples', 10);
+  map.set('Tomatoes', 10);
+  map.set('Pasts', 1);
+  map.set('Rice', 1);
+  map.set('Banana', 5);
+
+  return map;
 }
