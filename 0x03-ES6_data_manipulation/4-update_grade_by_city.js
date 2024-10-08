dae4868994f5ca8120a2385d5b750553bad96176
kase2228 <etelethiopia@gmail.com>
2024-10-08 04:20:53 +0300

    combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import getListStudents from "./0-get_list_students";
+
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+    const filterIt = arr.filter(studs => studs.location === city);
+    const mapIt = filterIt.map(studsInSan => {
+        const grades = newGrades.find(GRADE => GRADE.studentId === studsInSan.id );
+        return {
+            ...studsInSan, grade: grades ? grades.GRADE : 'N/A'
+        };
+
+    })
+}
