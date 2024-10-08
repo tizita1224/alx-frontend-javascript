8b1961bfd8eabec5a20793133eda7219c7fc7fb9
kase2228 <etelethiopia@gmail.com>
2024-10-08 00:02:39 +0000

    using filter()

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
-import getListStudents from "./0-get_list_students";
+import getListStudents from './0-get_list_students';
 
 export default function getStudentsByLocation(arr, city) {
-    const byLocation = arr.filter((Studs) => Studs.location == city);
-    return byLocation;
+  const byLocation = arr.filter((Studs) => Studs.location == city);
+  return byLocation;
 }
