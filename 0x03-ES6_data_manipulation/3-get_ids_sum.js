7ffb2a9a0b44645929612d03b83d811da1f3091f
kase2228 <etelethiopia@gmail.com>
2024-10-08 03:26:45 +0300

    using reduce

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import getListStudents from "./0-get_list_students";
+
+export default function getStudentIdsSum(arr) {
+    const idSum = arr.reduce((accumlator, studs) => accumlator + studs.id, 0)
+    return idSum;
+}
