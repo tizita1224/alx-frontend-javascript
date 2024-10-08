01db372ea2bab8c406c95de55473958b1cb8a1f8
kase2228 <tizitabelachew80@gmail.com>
2024-10-08 17:38:01 +0300

    clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+    
+    if (!startString || typeof startString !== 'string') {
+        return '';
+    }
+
+    const result = [...set]
+        .filter(value => value.startsWith(startString))
+        .map(value => value.slice(startString.length));
+
+    return result.join('-');
+}
